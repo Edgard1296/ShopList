0e67443f7c387f60903f2072b513812e1b264a14
Edgard Joaquin Valadez Aguilar <23470136@campeche.tecnm.mx>
2025-11-25 14:50:11 -0600

    Aplicacion totalmente funcional usando NVVM

--- ShopList.Gui/ViewModels/ShopListViewModel.cs
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -1,21 +1,89 @@
-﻿using ShopList.Gui.Models;
-using System;
-using System.Collections.Generic;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ShopList.Gui.Models;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace ShopList.Gui.ViewModels
 {
-    public class ShopListViewModel
-    {
-        public ObservableCollection<ShopListItem> ShopList { get;  }
+    public partial class ShopListViewModel : ObservableObject
 
-        public ShopListViewModel() 
+    {
+        [ObservableProperty]
+        private string _nombreDelArticulo = string.Empty;
+        [ObservableProperty]
+        private int _cantidad = 1;
+        [ObservableProperty]
+        private ShopListItem? _elementoSeleccionado = null;
+
+        public ObservableCollection<ShopListItem> ShopList { get; }
+
+        public ShopListViewModel()
         {
             ShopList = new ObservableCollection<ShopListItem>();
             CargarDatos();
+            if (ShopList.Count > 0)
+            {
+                ElementoSeleccionado = ShopList[0];
+            }
+            else
+            {
+                ElementoSeleccionado = null;
+            }
+        }
+
+        [RelayCommand]
+        public void AddShopListItem()
+        {
+            if (string.IsNullOrEmpty(NombreDelArticulo) || Cantidad <= 0)
+            {
+                return;
+            }
+
+            Random generador = new Random();
+            var item = new ShopListItem()
+            {
+                Id = generador.Next(),
+                Nombre = NombreDelArticulo,
+                Cantidad = this.Cantidad,
+                Comprado = false,
+            };
+            ShopList.Add(item);
+            ElementoSeleccionado = item;
+            NombreDelArticulo = string.Empty;
+            Cantidad = 1;
+        }
+
+        [RelayCommand]
+        public void RemoveShopListItem()
+        {
+            if (ElementoSeleccionado == null)
+            {
+                return;
+            }
+            ShopListItem? nuevoElementoSeleccionado;
+            int indice = ShopList.IndexOf(ElementoSeleccionado);
+            if (ShopList.Count > 1)
+            {
+                if (indice == ShopList.Count - 1)
+                {
+                    //Es le ultimo elemento
+                    nuevoElementoSeleccionado = ShopList[indice - 1];
+                }
+                else
+                {
+                    //No es el ultimo elemento
+                    nuevoElementoSeleccionado = ShopList[indice + 1];
+                }
+            }
+            else
+            {
+                //Es le unico elemento
+                nuevoElementoSeleccionado = null;
+            }
+            ShopList.Remove(ElementoSeleccionado);
+            ElementoSeleccionado = nuevoElementoSeleccionado;
         }
 
         private void CargarDatos()
@@ -23,31 +91,23 @@ namespace ShopList.Gui.ViewModels
             ShopList.Add(new ShopListItem()
             {
                 Id = 1,
-                Nombre = "Mayonesa",
+                Nombre = "Pan de caja",
                 Cantidad = 1,
                 Comprado = false
-
-
             });
-
             ShopList.Add(new ShopListItem()
             {
                 Id = 2,
                 Nombre = "Leche",
                 Cantidad = 3,
                 Comprado = false
-
-
             });
-
             ShopList.Add(new ShopListItem()
             {
                 Id = 3,
                 Nombre = "Queso",
                 Cantidad = 100,
                 Comprado = false
-
-
             });
         }
     }
